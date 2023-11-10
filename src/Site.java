b767c2db90f8cb70e387281557da7faaa06303ba
Rayatk <rayatkibria1999@gmail.com>
2023-11-10 14:46:29 -0500

    Made Site abstract

--- src/Site.java
+++ src/Site.java
@@ -1,6 +1,26 @@
-public class Site {
+public abstract class Site {
     public static double TAX_RATE = 0.05;
 
-    public Site() {
+    private double _units;
+    private double _rate;
+
+    public abstract double getBaseAmount();
+    public abstract double getTaxAmount();
+
+    public Site(double _units, double _rate) {
+        this._units = _units;
+        this._rate = _rate;
+    }
+
+    public double get_units() {
+        return _units;
+    }
+
+    public double get_rate() {
+        return _rate;
+    }
+
+    public double getBillableAmount() {
+        return getBaseAmount() + getTaxAmount();
     }
 }
